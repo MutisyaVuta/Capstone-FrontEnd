ab7e171b8cf8d57adce034d4d82cc315721a7199
louis mulaa <louismulaa06@gmail.com>
2024-08-09 17:13:40 +0300

    re worked on the navigation pages

--- frontend-app/src/components/louis/Trial.jsx
+++ frontend-app/src/components/louis/Trial.jsx
@@ -1,4 +1,4 @@
-// Appp.js
+//displays all the three admin pages
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Admin from "./Admin";
@@ -7,30 +7,15 @@ import Addbook from "./Addbook";
 
 const Trial = () => {
   return (
-    <Router>
-      <div className="container mt-4">
-        <div className="row">
-          <div className="col-md-12 text-right">
-            <h4>Welcome Admin</h4>
-          </div>
-        </div>
-        <div className="row mt-2">
-          <div className="col-md-12 text-right">
-            <Link to="/add-book" className="btn btn-primary btn-sm mr-2">
-              Add Books
-            </Link>
-            <Link to="/all-users" className="btn btn-secondary btn-sm">
-              All Users
-            </Link>
-          </div>
-        </div>
+    <div className="container mt-5">
+      <Router>
         <Routes>
           <Route path="/" element={<Admin />} />
           <Route path="/add-book" element={<Addbook />} />
           <Route path="/all-users" element={<Allusers />} />
         </Routes>
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 };
 
