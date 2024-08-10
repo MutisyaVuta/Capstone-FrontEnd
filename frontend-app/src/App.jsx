ced1be887e624744e5a5337589248ac7aff7786d
MutisyaVuta <nicholas.mutisya77@gmail.com>
2024-08-10 20:58:23 +0300

    nav

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -1,10 +1,11 @@
-import Dashboard from "./components/nick/Dashboard";
+import React from 'react';
+import Dashboard from './components/nick/Dashboard';
+
 function App() {
   return (
-    <>
-      <Dashboard name = "About" />
-      {/* Other routes */}
-    </>
+    <div className="App">
+      <Dashboard />
+    </div>
   );
 }
 
