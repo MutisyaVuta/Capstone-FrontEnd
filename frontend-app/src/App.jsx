c16e0fae44ea72f5e03b897987038f76333cf3de
Liam <lw433891@gmail.com>
2024-08-12 12:18:17 +0300

    'almost-done'

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -1,7 +1,10 @@
+import Loan from "./components/liam/Loan";
+
 function App() {
   return (
     <>
       <div>welcome to front end its already linked with bootstrap css</div>
+      <Loan />
     </>
   );
 }
